3253711e1609cbd9a58f704700423b4a0e9befbc
Magdalena Sztajdel <magdalena.sztajdel@britenet.com.pl>
2020-09-18 16:26:44 +0200

    texts and styles lesson

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, World</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.boldText}>Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.boldText}>Lorem ipsum dolor sit amet</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +19,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#6dcff6",
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "#c5efff",
+    padding: 20,
   },
 });
